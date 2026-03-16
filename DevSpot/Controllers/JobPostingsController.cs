8cef199ba17269a7646c0900752a977e0c267997
MahendherThatikonda <116361201+MahendherThatikonda@users.noreply.github.com>
2026-03-16 22:14:34 +1300

    Perform Delete operation using JQuery Ajax.

--- DevSpot/Controllers/JobPostingsController.cs
+++ DevSpot/Controllers/JobPostingsController.cs
@@ -1,11 +1,14 @@
 ﻿using DevSpot.Models;
 using DevSpot.Repository;
 using DevSpot.ViewModels;
+using DevSpot.Constants;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevSpot.Controllers
 {
+    [Authorize]
     public class JobPostingsController : Controller
     {
 
@@ -20,17 +23,20 @@ namespace DevSpot.Controllers
             _repository = repository;
             _userManager = userManager;
         }
+        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
             var jobPostings = await _repository.GetAllSync();
             return View(jobPostings);
         }
 
+        [Authorize(Roles ="Admin, Employer")]
         public IActionResult Create()
         {
             return View();
         }
 
+        [Authorize]
         [HttpPost]
         public async Task<IActionResult> Create(JobPostingViewModel jobPostingvm)
         {
@@ -52,6 +58,28 @@ namespace DevSpot.Controllers
             }
             // 
             return View(jobPostingvm);
+        }
+
+        [HttpDelete]
+        [Authorize(Roles = "Admin, Employer")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var jobPosting = await _repository.GetbyIdAsync(id);
+            if (jobPosting == null)
+            {
+                return NotFound();
+            }
+
+            var userId = _userManager.GetUserId(User);
+            if (User.IsInRole(Roles.Admin) == false && jobPosting.UserId!=userId)
+            {
+                return Forbid();
+            }
+
+            await _repository.DeleteASync(id);
+
+            return Ok();
+
         }
     }
 }
